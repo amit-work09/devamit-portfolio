da685c07dbb4005c51e1c7df184012c210f7f199
Amit Sharma <work.amit09@gmail.com>
2024-08-10 12:24:00 +0530

    Resume Update

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/AMIT_SHARMA_CV.pdf";
+import pdf from "../../Assets/../Assets/Amit_Sharma_CV_082024.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
